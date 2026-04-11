bd95d9b02fbbaa126805038e9a686c228b38dff6
tg117official <technologicalgeeks117@gmail.com>
2026-04-11 08:58:41 +0530

    Update data-transformation.py

--- data-transformation.py
+++ data-transformation.py
@@ -1,3 +1,5 @@
 data = ["row1", "row2", "row3"]
 transformed = [d.replace("row", "item") for d in data]
 print("Transformed:", transformed)
+transformed _data.write(DEST)
+print("Data Written Successfully")
